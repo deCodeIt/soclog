5016f2b484384993a2388b13aff9e1e77e9480df
deCodeIt <786alijafri786@gmail.com>
2016-01-23 18:53:38 +0530

    css changed, console output reduced

--- account.php
+++ account.php
@@ -23,8 +23,8 @@
 
 function testAPI(response){
     $(".social_login").hide();
-    console.log(response.authResponse.accessToken);
-    console.log(JSON.stringify(response));
+    // console.log(response.authResponse.accessToken);
+    // console.log(JSON.stringify(response));
     
     //getting user details
     FB.api('/me?fields=id,email,name,gender', function(resp) {
@@ -45,21 +45,21 @@ function testAPI(response){
     });
     //getting user profile pic
     FB.api('/me/picture?type=small', function(response) {
-        console.log(JSON.stringify(response));
-        // document.getElementById('pPic').src=response.data.url;
+        // console.log(JSON.stringify(response));
+        document.getElementById('pPic').src=response.data.url;
     });
 }
 
 // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
+    // console.log('statusChangeCallback');
+    // console.log(response);
     // The response object is returned with a status field that lets the
     // app know the current login status of the person.
     // Full docs on the response object can be found in the documentation
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
-      console.log('Logged into app and connected');
+      // console.log('Logged into app and connected');
       // Logged into your app and Facebook.
       testAPI(response);
     } else if (response.status === 'not_authorized') {
@@ -101,8 +101,8 @@ function testAPI(response){
   function fbLogin(){
         FB.getLoginStatus(function(response) {
                 if (response.status === 'connected') {
-                        console.log(JSON.stringify(response));
-                        console.log('Logged in.');
+                        // console.log(JSON.stringify(response));
+                        // console.log('Logged in.');
                         testAPI(response);
                 }
                 else {
@@ -185,7 +185,7 @@ function testAPI(response){
 </div>
 
 <script type="text/javascript">
-        $("#modal_trigger").leanModal({top : 150, overlay : 0.6, closeButton: ".modal_close" });
+        $("#modal_trigger").leanModal({top : 50, overlay : 0.6, closeButton: ".modal_close" });
 
         $(function(){
                 // Calling Login Form
