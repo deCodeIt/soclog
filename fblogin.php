78cad7e70714128be669ea8ad9a0f2336d512b19
deCodeIt <786alijafri786@gmail.com>
2016-01-23 09:55:28 +0530

    t1

--- fblogin.php
+++ fblogin.php
@@ -94,6 +94,13 @@
 
 <fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
 </fb:login-button>
+<div
+  class="fb-like"
+  data-share="true"
+  data-width="450"
+  data-show-faces="true">
+</div>
+<button 
 
 <div id="status">
 </div>
