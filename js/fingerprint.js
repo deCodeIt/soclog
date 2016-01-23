f5620abf593dff4e5c44aa734c3e22291fa0aabd
deCodeIt <786alijafri786@gmail.com>
2016-01-23 23:56:11 +0530

    Adding wait animation

--- js/fingerprint.js
+++ js/fingerprint.js
@@ -24,6 +24,7 @@ $(document).ready(function(){
         //for event registration
         $(document).on('click', '.event-reg', function() {
         obj =this;
+        $(obj).html('<img src="images/ajax-loading.gif" style="width:38px;height:38px">');
         console.log('event clicked');
         var status = 2;
         if( $(this).hasClass('reg'))
@@ -95,6 +96,12 @@ $(document).ready(function(){
             }
             else
             {
+                //waiting logo
+                $('div.registered-events').show();
+                $('ul#registered-events').html(
+                    '<li><img src="images/ajax-loading.gif" style="width:38px;height:38px"></li>';
+                    );
+                //sending request
                 $.ajax({
                   method: 'POST',
                   url: 'myevents.php',
