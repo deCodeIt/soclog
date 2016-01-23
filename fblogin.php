2b9001a0c6f19a90dfcf3b5dc8888745fe4f98ad
deCodeIt <786alijafri786@gmail.com>
2016-01-23 11:14:34 +0530

    implementing Facebook Javascript SDK

--- fblogin.php
+++ fblogin.php
@@ -8,7 +8,7 @@
 function testAPI(response){
   console.log(response.authResponse.accessToken);
   console.log(JSON.stringify(response));
-  FB.api('/me?field=email,id,gender,name', function(response) {
+  FB.api('/me?fields=email,id,gender,name', function(response) {
     console.log(JSON.stringify(response));
     document.getElementById('status').innerHTML=JSON.stringify(response);
   });
