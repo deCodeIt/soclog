577a3445a64d5e95a77971849cd4fe08633bb0a4
deCodeIt <786alijafri786@gmail.com>
2016-01-23 11:55:14 +0530

    Merging FB with Original account.php

--- account.php
+++ account.php
@@ -7,6 +7,77 @@
 <link type="text/css" rel="stylesheet" href="css/style.css" />
 
 <body>
+<!--facebook login Integration -->
+<script>
+
+function testAPI(response){
+  console.log(response.authResponse.accessToken);
+  console.log(JSON.stringify(response));
+  FB.api('/me?fields=id,email,name,gender', function(response) {
+    console.log(JSON.stringify(response));
+    document.getElementById('status').innerHTML=JSON.stringify(response);
+});
+  FB.api('/me/picture?type=small', function(response) {
+    console.log(JSON.stringify(response));
+    document.getElementById('pPic').src=response.data.url;
+});
+}
+
+// This is called with the results from from FB.getLoginStatus().
+  function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      console.log('Logged into app and connected');
+      // Logged into your app and Facebook.
+      testAPI(response);
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into Facebook.';
+    }
+  }
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '991993504180041',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+  function fbLogin(){
+    FB.login(function(response) {
+        statusChangeCallback(response);
+        // handle the response
+    }, {
+      scope: 'email', 
+      return_scopes: true
+    });
+  }
+  
+
+</script>
+<div id="status"></div>
+<img src="#" id="pPic" />
+<!-- facebook login integration script closed-->
 <div class="container">
         <a id="modal_trigger" href="#modal" class="btn">Event</a>
 
@@ -20,7 +91,7 @@
                         <!-- Social Login -->
                         <div class="social_login">
                                 <div class="">
-                                        <a href="#" class="social_box fb">
+                                        <a href="#" onclick="fbLogin()" class="social_box fb">
                                                 <span class="icon"><i class="fa fa-facebook"></i></span>
                                                 <span class="icon_title">Connect with Facebook</span>
                                                 
