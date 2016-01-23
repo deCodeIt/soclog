39ec10e75100b84e309ed830fccafc55b3169f24
deCodeIt <786alijafri786@gmail.com>
2016-01-23 20:40:54 +0530

    form input

--- register.php
+++ register.php
@@ -12,6 +12,33 @@ function setup($field){
 	}
 }
 
+if(setup('email') && setup('location') && setup('college'))
+{
+	$email = mysqli_real_escape_string($connect,htmlentities($_REQUEST['email']));
+	$location = mysqli_real_escape_string($connect,htmlentities($_REQUEST['location']));
+	$college = mysqli_real_escape_string($connect,htmlentities($_REQUEST['college']));
 
+	if($_SESSION['id']==NULL)
+	{
+		//user is trying to fraud with the system
+		return NULL;
+	}
+
+	$myquery=sprintf("UPDATE %s SET email='%s',location='%s',college='%s' WHERE id='%s'",$table_student,$email,$location,$college,$_SESSION['id']);
+	if($resQuery=mysqli_query($connect,$myquery))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+
+
+}
+else
+{
+	return NULL;
+}
 
 ?>
