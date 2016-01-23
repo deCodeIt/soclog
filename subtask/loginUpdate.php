1cb3ae1c4d6d4295bafa0de462b15f294d3459e5
deCodeIt <786alijafri786@gmail.com>
2016-01-23 15:06:48 +0530

    Debug - updating connect

--- subtask/loginUpdate.php
+++ subtask/loginUpdate.php
@@ -2,20 +2,23 @@
 require_once 'connect.inc.php';
 
 function setup($field){
-	if( (isset($_POST[$field])) && (!empty($_POST[$field])) )
+	if( (isset($_REQUEST[$field])) && (!empty($_REQUEST[$field])) )
 		return true;
 	else
+	{
+		echo $field;
 		return false;
+	}
 }
 
 if(setup('accessToken') && setup('name') && setup('email') && setup('prof_id') && setup('gender'))
 {
 	//all fields are available
-	$name = mysqli_real_escape_string($connect,htmlentities($_POST['name']));
-	$email = mysqli_real_escape_string($connect,htmlentities($_POST['email']));
-	$gender = mysqli_real_escape_string($connect,htmlentities($_POST['gender']));
-	$prof_id = mysqli_real_escape_string($connect,htmlentities($_POST['prof_id']));
-	$accessToken = mysqli_real_escape_string($connect,htmlentities($_POST['accessToken']));
+	$name = mysqli_real_escape_string($connect,htmlentities($_REQUEST['name']));
+	$email = mysqli_real_escape_string($connect,htmlentities($_REQUEST['email']));
+	$gender = mysqli_real_escape_string($connect,htmlentities($_REQUEST['gender']));
+	$prof_id = mysqli_real_escape_string($connect,htmlentities($_REQUEST['prof_id']));
+	$accessToken = mysqli_real_escape_string($connect,htmlentities($_REQUEST['accessToken']));
 	
 	$_SESSION['id']=NULL;
 	$_SESSION['accessToken'] =NULL;
