ad902fd88c3e60d5f7fd393c7a945d5097199bca
deCodeIt <786alijafri786@gmail.com>
2016-01-23 10:45:51 +0530

    Reverting back to fb Javascript SDK

--- fblogin.php
+++ fblogin.php
@@ -1,15 +1,4 @@
 <?php
 require_once './subtask/fb-path.inc.php';
-$fb = new Facebook\Facebook([
-  'app_id' => '1521684931494498',
-  'app_secret' => '543706019bf2511bfd88a1977e542661',
-  'default_graph_version' => 'v2.2',
-  ]);
 
-$helper = $fb->getRedirectLoginHelper();
-
-$permissions = ['email','public_profile']; // Optional permissions
-$loginUrl = $helper->getLoginUrl('http://worldis4u.xyz/soclog/fb-callback.php', $permissions);
-
-echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
 ?>
--- subtask/fb-path.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-require_once __DIR__ . '/../facebook-php-sdk/src/Facebook/autoload.php';
-?>
