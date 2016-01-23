6d3536b965cd28ba4f0e5b01c4dff42a0bd84488
deCodeIt <786alijafri786@gmail.com>
2016-01-23 22:25:48 +0530

    Scripts file separated

---
+++ js/fb.js
@@ -0,0 +1,105 @@
+function testAPI(response){
+    
+    // console.log(response.authResponse.accessToken);
+    // console.log(JSON.stringify(response));
+    
+    //getting user details
+    FB.api('/me?fields=id,email,name,gender', function(resp) {
+        // console.log(JSON.stringify(resp));
+        //Updating data with the server
+        $.ajax({
+          method: "POST",
+          url: "subtask/loginUpdate.php",
+          data: { accessToken:response.authResponse.accessToken, name: resp.name , prof_id: resp.id, email: resp.email, gender: resp.gender }
+        })
+          .done(function( msg ) {
+            // console.log("DONE");
+            // console.log(msg);
+            $(".social_login").hide();
+            $(".user_register").show();
+            $(".user_register #name").val(resp.name);
+            if(resp.email)
+                $(".user_register #email").val(resp.email);
+
+        });
+    });
+    //getting user profile pic
+    FB.api('/me/picture?type=square', function(response) {
+        // console.log(JSON.stringify(response));
+        document.getElementById('pPic').src=response.data.url;
+        $( "#pPic" ).load( response.data.url, function() {
+        $('#div4').show();
+        });
+
+    });
+}
+
+// This is called with the results from from FB.getLoginStatus().
+  function statusChangeCallback(response) {
+    // console.log('statusChangeCallback');
+    // console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // console.log('Logged into app and connected');
+      // Logged into your app and Facebook.
+      testAPI(response);
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into Facebook.';
+    }
+  }
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '991993504180041',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+                // console.log('Logged in.');
+                //isLoggedIn();
+                testAPI(response);
+        }
+        });
+  };
+
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+  function fbLogin(){
+        FB.getLoginStatus(function(response) {
+                if (response.status === 'connected') {
+                        // console.log(JSON.stringify(response));
+                        // console.log('Logged in.');
+                        // testAPI(response);
+                }
+                else {
+                        FB.login(function(response) {
+                                statusChangeCallback(response);
+                        // handle the response
+                        }, {
+                                scope: 'email', 
+                                return_scopes: true
+                        });
+                }
+        });
+  }
+
+  
---
+++ js/fingerprint.js
@@ -0,0 +1,129 @@
+$(document).ready(function(){
+        //for registration
+        $(document).on('submit', 'form#registration_form', function() {            
+        $.ajax({
+          method: $(this).attr('method'),
+          url: $(this).attr('action'),
+          data: $(this).serialize()
+        })
+          .done(function( msg ) {
+            // console.log("YES");
+            // console.log(msg);
+            // console.log(JSON.parse(msg));
+            data = JSON.parse(msg);
+            if(data.status=='true')
+            {
+                $("#user_register").hide();
+                $("event_pg").show();
+                $(".modal_close").click();
+            }
+
+        });    
+        return false;
+    });
+        //for event registration
+        $(document).on('click', '.event-reg', function() {
+        obj =this;
+        console.log('event clicked');
+        var status = 2;
+        if( $(this).hasClass('reg'))
+        {
+                status = 1;
+        }
+        dat={'zeit_event':$(this).attr('href').substr(1),'stat':status};
+        console.log(dat);
+        $.ajax({
+          method: 'POST',
+          url: 'event_reg.php',
+          data: dat
+        })
+          .done(function( msg ) {
+            // console.log("YES");
+            // console.log(msg);
+            // console.log(JSON.parse(msg));
+            data = JSON.parse(msg);
+            if(data.status=='true')
+            {
+                if(data.reg==1)
+                {
+                        $(obj).addClass('reg');
+                        $(obj).html('Registered');
+                }
+                else
+                 {      $(obj).removeClass('reg');
+                        $(obj).html('Register');
+                 }
+            }
+
+        });    
+        return false;
+    });
+
+        //event current status
+        //for event registration
+        $('.event-reg').each(function(index) {
+        obj =this;
+        dat={'zeit_event':$(this).attr('href').substr(1)};
+        $.ajax({
+          method: 'POST',
+          url: 'event_status.php',
+          data: dat
+        })
+          .done(function( msg ) {
+            data = JSON.parse(msg);
+            if(data.status=='true')
+            {
+                if(data.reg==1)
+                {
+                        $(obj).addClass('reg');
+                        $(obj).html('Registered');
+                }
+                else
+                 {      $(obj).removeClass('reg');
+                        $(obj).html('Register');
+                 }
+            }
+
+        });
+    });
+
+
+});
+
+
+function validateForm(){
+        $(function() {
+            // validate and process form here
+              
+                $('.error').removeClass('error');
+
+                var field = $("input#email");
+                var status=true;
+                if (field.val() == "") {
+                        field.addClass('error');
+                        field.focus();
+                        status = false;
+                      }
+                field = $("input#college");
+                if (field.val() == "") {
+                        field.addClass('error');
+                        field.focus();
+                        status = false;
+                      }
+                field = $("input#location");
+                if (field.val() == "") {
+                        field.addClass('error');
+                        field.focus();
+                        status = false;
+                      }
+                field = $("input#tos");
+                if(!field.is(':checked'))
+                {
+                        field.addClass('error');
+                        status=false;
+                }
+                //validation passed
+                if(status==true)
+                        $('form#registration_form').submit();
+          });
+}
